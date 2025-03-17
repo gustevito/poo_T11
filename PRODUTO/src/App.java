0d1a660571562555c675bec86bf83084ceb6ef22
Gustavo Flores <108235783+gustevito@users.noreply.github.com>
2025-03-17 14:07:04 +0000

    errado ainda

--- PRODUTO/src/App.java
+++ PRODUTO/src/App.java
@@ -13,25 +13,37 @@ public class App {
         int somaVendas = 0;
         int itensVendidos = 0;
 
+        int qtd = 0;
+        int totalVendas = 0;
+
         for(int i = 0; i < vendas.length; i++){
             somaVendas += vendas[i].getPrecoFinal();
             itensVendidos += vendas[i].getPrecoFinal();
         }
 
-        // formatação textual da moeda
         NumberFormat formatoMoeda = NumberFormat.getCurrencyInstance(Locale.of("pt", "BR"));
         String somatorioFormatado = formatoMoeda.format(somaVendas); 
 
-        // out
+
+        // ta tudo errado ainda kkkk mas to tentando resolver.
+        System.out.println("-------------------------------"); // produtos e vendas
+        for(int i = 0; i < produtos.length; i++){
+            int codProduto = produtos[i].getCodigo();
+                for (int j = 0; j < vendas.length; j++){
+                    if (vendas[j].getCodigoProduto() == codProduto){
+                        qtd += vendas[j].getQuantidade();
+                    }
+                } 
+            System.out.println(produtos[i].toString() + "(Qtd.: " + qtd + " / Total: " + vendas[i].getPrecoFinal() + ")");
+        }
+        System.out.println("-------------------------------"); // somatorio + total
+
         System.out.println("Soma total das vendas: " + somatorioFormatado);
         System.out.println("Total de itens vendidos: " + itensVendidos);
-        
-        // divisão gambiarrenta
-        System.out.println("-------------------------------");
 
-        // produtos
-        for(int i = 0; i < produtos.length; i++){
-            System.out.println(produtos[i].toString());
-        }
+        System.out.println("-------------------------------"); // total de unidades + valor total de cada produto   
+
+        System.out.println();
+
     }
 }
